a5824813e0c2c70e842f82f1e9ed080ea9f45a0f
bohdanbatsenko <batsenko_b@ukr.net>
2021-12-02 22:33:07 +0200

    11

--- app/js/app.js
+++ app/js/app.js
@@ -5,4 +5,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
 
+	//header-menu
+	const mMenu = document.querySelector('.m-menu');
+	const menu = document.querySelector('.menu');
+	mMenu.addEventListener('click', (event) => {
+		mMenu.classList.toggle("open");
+		menu.classList.toggle('menu-open');
+	});
+
+
+
+
+	const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]');
+	const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
+
+	for (const tab of tabsHandlerElems) {
+		tab.addEventListener('click', ()=> {
+			tabsHandlerElems.forEach(item =>{
+				if (tab === item) {
+					item.classList.add('tabs-list__item_active');
+				} else {
+					item.classList.remove('tabs-list__item_active');
+				}
+			})
+			tabsFieldElems.forEach(item => {
+				if (item.dataset.tabsField === tab.dataset.tabsHandler) {
+					item.classList.remove('hidden')
+				} else {
+					item.classList.add('hidden')
+				}
+			})
+		})
+	}	
+
 })
